e87062aba1d7e9135e5ea5d7024638060c571408
Juho Haimakainen <juhohaima@gmail.com>
2026-04-02 14:16:24 +0300

    Yleisiä muutoksia ja lisäyksiä

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import "./Hero.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Hero = () => {
   return (
@@ -10,12 +13,21 @@ const Hero = () => {
         <div className="hero-main-flex">
           <div className="hero-left">
             <h1>Hei olen Juho!</h1>
+            
             <div className="hero-socials">
-              <a href="#" target="_blank" className="social-icon">LinkedIn</a>
-              <a href="#" target="_blank" className="social-icon">GitHub</a>
-              <a href="#" target="_blank" className="social-icon">Gmail</a>
+             
+              <a href="#" target="_blank" className="social-icon" aria-label="LinkedIn">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a href="#" target="_blank" className="social-icon" aria-label="GitHub">
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a href="#" target="_blank" className="social-icon" aria-label="Gmail">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </a>
             </div>
           </div>
+
           <p>
             Olen 17-vuotias opiskelija Oulusta. Opiskelen OSAO:lla Tieto- ja Viestintätekniikan perustutkintoa, ja tulen valmistumaan ohjelmistokehittäjäksi Kesäkuussa 2027.
           </p>
